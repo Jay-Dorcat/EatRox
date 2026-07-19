203a24a1fa308817ff40b83dabb1067482f4d0b3
Ray4Cloud <raymond4cloud@gmail.com>
2026-07-19 17:13:55 +0200

    Added laser lasering
    - Laser shoots now when colliding with walls
    - for some reason, it shoots in weird directions
    - I need lot's of coffee...

--- Entities/Laser/laser.gd
+++ Entities/Laser/laser.gd
@@ -1,12 +1,19 @@
 extends Node2D
-
+var shooting: bool = false
+var wall: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	shooting = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$LaserBeam.add_point(Vector2(0,0))
+	if shooting:
+		$LaserBeam.add_point(Vector2(0,0))
+		if $RayCast2D.is_colliding():
+			wall = $RayCast2D.get_collision_point()
+			$LaserBeam.add_point(wall)
+			
+		
 	# Oh my god, BRAIN PLEASEEE
